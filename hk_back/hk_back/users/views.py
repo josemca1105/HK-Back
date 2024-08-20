0f332e9676475fd8608e2fd2f0f44d3c9f86ae00
josemca105 <jm.calderon1105@gmail.com>
2024-08-20 10:42:45 -0400

    Creadas vistas login, getuser y logout de usuarios

--- hk_back/hk_back/users/views.py
+++ hk_back/hk_back/users/views.py
@@ -10,6 +10,8 @@ from .serializers import UserSerializer
 import jwt, datetime
 
 # Create your views here.
+
+# RegisterView class is used to register a new user
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -21,5 +23,63 @@ class RegisterView(APIView):
             'status': status.HTTP_200_OK,
             'message': 'User registered successfully',
             'data': serializer.data
+        }
+        return response
+    
+# LoginView class is used to login a user
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.now(datetime.UTC)
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'status': status.HTTP_200_OK,
+            'message': 'User logged in successfully',
+            'token': token
+        }
+        return response
+    
+# UserView class is used to get the user details
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+    
+# LogoutView class is used to logout a user
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'status': status.HTTP_200_OK,
+            'message': 'User logged out successfully'
         }
         return response
