fcb57068ec9485eeaa82eb4b8d0395f872dd56a4
josemca105 <jm.calderon1105@gmail.com>
2024-08-22 11:42:40 -0400

    Reordenamiento clases usuarios. Creados metodos para inmuebles

--- hk_back/hk_back/inmuebles/views.py
+++ hk_back/hk_back/inmuebles/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.validators import ValidationError
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+
+from .models import Inmuebles
+from hk_back.users.models import User
+from .serializers import InmueblesSerializer
+
+import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
+
+from django.conf import settings
+
+JWT_SECRET = settings.SECRET_KEY
 
 # Create your views here.
+
+# InmueblesPersonalView class is used to get inmuebles of the logged in user
+class InmueblesPersonalView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        try:
+            payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        except (ExpiredSignatureError, InvalidSignatureError):
+            raise AuthenticationFailed('Unauthenticated')
+        
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        
+        serializer = InmueblesSerializer(Inmuebles.objects.filter(asesor=user), many=True)
+        return Response(
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+# InmueblesAllView class is used to get all inmuebles
+class InmueblesAllView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        try:
+            jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        except (ExpiredSignatureError, InvalidSignatureError):
+            raise AuthenticationFailed('Unauthenticated')
+        
+        serializer = InmueblesSerializer(Inmuebles.objects.all(), many=True)
+        return Response(
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+# InmueblesCreateView class is used to create a new inmueble
+class InmueblesCreateView(APIView):
+    def post(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        try:
+            payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        except (ExpiredSignatureError, InvalidSignatureError):
+            raise AuthenticationFailed('Unauthenticated')
+        
+        user = User.objects.filter(id=payload['id']).first()
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        
+        data = request.data.copy()
+        data['asesor'] = user.id
+
+        serializer = InmueblesSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(asesor=user)
+
+        response = Response()
+        response.data = {
+            'status': status.HTTP_200_OK,
+            'message': 'Inmueble creado exitosamente',
+            'data': serializer.data
+        }
+        return response
--- hk_back/hk_back/users/urls.py
+++ hk_back/hk_back/users/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
-from .views import RegisterView, LoginView, AuthView, LogoutView, UsersView
+from .views import (
+    RegisterView,
+    LoginView,
+    AuthView,
+    LogoutView,
+    UsersListView,
+    UserCreateView,
+    UserDetailView
+)
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('user', AuthView.as_view(), name='user'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('users', UsersView.as_view(), name='users_api'),
-    path('users-create', UsersView.as_view(), name='users_create_api'),
-    path('users-update/<int:id>', UsersView.as_view(), name='users_update_api'),
-    path('users-delete/<int:id>', UsersView.as_view(), name='users_delete_api'),
+    path('users', UsersListView.as_view(), name='users_api'),
+    path('users-create', UserCreateView.as_view(), name='users_create_api'),
+    path('users-update/<int:id>', UserDetailView.as_view(), name='users_update_api'),
+    path('users-delete/<int:id>', UserDetailView.as_view(), name='users_delete_api'),
 ]
